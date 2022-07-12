672cb7bc2155fc001806e9ee45d42cca4651cba0
Anfisa <anfisa.la@gmail.com>
2022-07-12 23:09:59 +0300

    add file class

--- file.py
+++ file.py
@@ -3,13 +3,16 @@ class File:
     def __init__(self, file_dir):
         self.file_dir = file_dir
 
-    def open_file(self):
+    def open(self):
         with open(self.file_dir, mode='r', encoding='latin1') as file:
             return file.read()
 
     def rtf_to_text(self):
         from striprtf.striprtf import rtf_to_text
-        rtf = self.open_file()
+        rtf = self.open()
         text = rtf_to_text(rtf)
         print(text)
+
+
+
 
