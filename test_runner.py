3ee8d74b6213d97fc8f90c495dfb662470b80d6a
Anfisa <anfisa.la@gmail.com>
2022-07-13 20:15:59 +0300

    add tests

--- test_runner.py
+++ test_runner.py
@@ -7,12 +7,17 @@ direction = '/Users/anfisa/PycharmProjects/pythonProject/kata'
 class TestMyRunner(unittest.TestCase):
 
     def test_no_allure(self):
-        self.assertEqual(my_test_runner.create_test_command(direction, False),
+        self.assertEqual(my_test_runner.create_test_command(direction, False, False),
                          'pytest /Users/anfisa/PycharmProjects/pythonProject/kata'
                          )
 
+    def test_no_allure_stdout_to_file(self):
+        self.assertEqual(my_test_runner.create_test_command(direction, False, True),
+                         'pytest /Users/anfisa/PycharmProjects/pythonProject/kata > /Users/anfisa/Desktop/test_log.txt'
+                         )
+
     def test_use_allure(self):
-        self.assertEqual(my_test_runner.create_test_command(direction, True),
+        self.assertEqual(my_test_runner.create_test_command(direction, True, False),
                          'pytest /Users/anfisa/PycharmProjects/pythonProject/kata --alluredir=results'
                          )
 
