bade689e2cd6a6fd10530ba6a0fafaaef51448ad
Anfisa <anfisa.la@gmail.com>
2022-07-13 20:15:51 +0300

    stdout -> txt

--- my_test_runner.py
+++ my_test_runner.py
@@ -1,14 +1,19 @@
 import command_runner
 import scheduler
+import sys
 
 direction = '/Users/anfisa/PycharmProjects/pythonProject/kata'
+txt_file = '/Users/anfisa/Desktop/test_log.txt'
 allure_result_dir = 'results'
-use_allure = True
+
+use_allure = False
+stdout_to_file = True
 
 
-def create_test_command(direction: str, use_allure: bool):
+def create_test_command(direction: str, use_allure: bool, stdout_to_file: bool):
     allure = ' --alluredir={}'.format(allure_result_dir) if use_allure else ''
-    return 'pytest {}{}'.format(direction, allure)
+    stdout = ' > {}'.format(txt_file) if stdout_to_file else ''
+    return 'pytest {}{}{}'.format(direction, allure, stdout)
 
 
 def create_report_command(allure_result_dir):
@@ -16,7 +21,7 @@ def create_report_command(allure_result_dir):
 
 
 def run_tests():
-    command_runner.run_command(create_test_command(direction, use_allure))
+    command_runner.run_command(create_test_command(direction, use_allure, stdout_to_file))
 
 
 def create_report():
@@ -28,9 +33,13 @@ def run():
         run_tests()
         create_report()
     else:
-        run_tests()
+        sys.stdout = open(txt_file, "w")
+        print(run_tests())
 
 
 def schedule_run():
     scheduler.main(run, '21:00')
+
+
+run()
 
