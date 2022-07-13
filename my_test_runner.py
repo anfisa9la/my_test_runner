0800c286b689b0af44742d6f687a6c3ab0a53ee7
Anfisa <anfisa.la@gmail.com>
2022-07-13 23:35:27 +0300

    fix run

--- my_test_runner.py
+++ my_test_runner.py
@@ -10,7 +10,7 @@ use_allure = False
 stdout_to_file = True
 
 
-def create_test_command(direction: str, use_allure: bool, stdout_to_file: bool):
+def create_test_command(direction, use_allure: bool, stdout_to_file: bool):
     allure = ' --alluredir={}'.format(allure_result_dir) if use_allure else ''
     stdout = ' > {}'.format(txt_file) if stdout_to_file else ''
     return 'pytest {}{}{}'.format(direction, allure, stdout)
@@ -32,9 +32,11 @@ def run():
     if use_allure:
         run_tests()
         create_report()
-    else:
+    elif stdout_to_file:
         sys.stdout = open(txt_file, "w")
         print(run_tests())
+    else:
+        run_tests()
 
 
 def schedule_run():
