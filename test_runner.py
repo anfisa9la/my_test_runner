a2269448210a1c7bd17f9b6e8cf3e2ad51db5d2c
Anfisa <anfisa.la@gmail.com>
2022-07-15 22:33:18 +0300

    fix tests

--- test_runner.py
+++ test_runner.py
@@ -1,25 +1,27 @@
 import unittest
+
+import configuration
 import my_test_runner as runner
 
-direction = '/Users/anfisa/PycharmProjects/pythonProject/kata'
+direction = '/Users/anfisa/test'
 
 
 class TestMyRunner(unittest.TestCase):
 
     def test_no_allure(self):
         self.assertEqual(runner.create_test_command(direction, False, False),
-                         'pytest /Users/anfisa/PycharmProjects/pythonProject/kata'
+                         'pytest /Users/anfisa/test'
                          )
 
     def test_no_allure_stdout_to_file(self):
         self.assertEqual(runner.create_test_command(direction, False, True),
-                         'pytest /Users/anfisa/PycharmProjects/pythonProject/kata > {}'.format(runner.txt_file)
+                         'pytest /Users/anfisa/test > {}'.format(runner.txt_file)
                          )
 
     def test_use_allure(self):
         self.assertEqual(runner.create_test_command(direction, True, False),
-                         'pytest /Users/anfisa/PycharmProjects/pythonProject/kata --alluredir={}'.format(
-                             runner.allure_result_dir)
+                         'pytest /Users/anfisa/test --alluredir={}'.format(
+                             configuration.allure_result_dir)
                          )
 
     def test_allure_creation(self):
